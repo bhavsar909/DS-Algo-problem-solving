a547393e09277c0898a58e5f9c153ae963604ba2
“rajbhavsar” <raj.bhavsar@brevitaz.com>
2020-05-04 10:09:21 +0530

    Test code

---
+++ src/main/java/com/rajbhavsar/basic_ds/Test.java
@@ -0,0 +1,16 @@
+package com.rajbhavsar.basic_ds;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		String[] s = new String[10];
+		s[2]="hello";
+		for (int i = 0; i < s.length ; i++)
+		{
+			System.out.println("xxxxx"+s[i]);
+
+		}
+
+	}
+}
